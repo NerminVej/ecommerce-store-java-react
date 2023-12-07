6496e3a7628f2c92cb9c5d0522b6addb67f0df32
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-12-07 18:02:05 +0100

    created the java spring project

---
+++ ecommerce-store-java/src/main/java/com/example/ecommercestorejava/EcommerceStoreJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercestorejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceStoreJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceStoreJavaApplication.class, args);
+	}
+
+}
---
+++ ecommerce-store-java/src/test/java/com/example/ecommercestorejava/EcommerceStoreJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommercestorejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceStoreJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
