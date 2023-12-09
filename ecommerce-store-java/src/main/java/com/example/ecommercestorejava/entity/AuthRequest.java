2d82c29114e05831a1c744dd0c6bce8d491fdfe7
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-12-09 12:26:10 +0100

    fully implemented the AuthRequest entity

--- ecommerce-store-java/src/main/java/com/example/ecommercestorejava/entity/AuthRequest.java
+++ ecommerce-store-java/src/main/java/com/example/ecommercestorejava/entity/AuthRequest.java
@@ -1,4 +1,15 @@
 package com.example.ecommercestorejava.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthRequest {
+
+    private String username;
+    private String password;
+
 }
