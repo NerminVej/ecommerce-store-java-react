ef49c0b69c4589e21388a36630f72fa8ed2c228f
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-12-10 16:15:17 +0100

    created the full interface for the userinfo

--- ecommerce-store-java/src/main/java/com/example/ecommercestorejava/repository/UserInfoRepository.java
+++ ecommerce-store-java/src/main/java/com/example/ecommercestorejava/repository/UserInfoRepository.java
@@ -1,4 +1,13 @@
 package com.example.ecommercestorejava.repository;
 
-public interface UserInfoRepository {
+
+import com.example.ecommercestorejava.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByName(String username);
 }
