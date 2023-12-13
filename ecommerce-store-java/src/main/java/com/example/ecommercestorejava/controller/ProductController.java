0b30bcc6355a939d9a64f0f5ff73926e7d56e750
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-12-13 10:23:08 +0100

    fully implemented the productcontroller class with the get mapping

--- ecommerce-store-java/src/main/java/com/example/ecommercestorejava/controller/ProductController.java
+++ ecommerce-store-java/src/main/java/com/example/ecommercestorejava/controller/ProductController.java
@@ -1,4 +1,27 @@
 package com.example.ecommercestorejava.controller;
 
+
+
+import com.example.ecommercestorejava.entity.Product;
+import com.example.ecommercestorejava.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping(value = { "", "/" })
+    public @NotNull Iterable<Product> getProducts() {
+        return productService.getAllProducts();
+    }
 }
