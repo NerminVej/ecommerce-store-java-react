a5f044e5c7e7b578fe5f7fa416e7621ba89e841c
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-12-17 08:59:22 +0100

    created the UserService class

---
+++ ecommerce-store-java/src/main/java/com/example/ecommercestorejava/service/UserService.java
@@ -0,0 +1,41 @@
+package com.example.ecommercestorejava.service;
+
+
+import java.time.LocalDateTime;
+
+import com.example.ecommercestorejava.entity.User;
+import com.example.ecommercestorejava.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) {
+                return userRepository.findByEmail(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            }
+        };
+    }
+
+    public User save(User newUser) {
+        if (newUser.getId() == null) {
+            newUser.setCreatedAt(LocalDateTime.now());
+        }
+
+        newUser.setUpdatedAt(LocalDateTime.now());
+        return userRepository.save(newUser);
+    }
+
+}
