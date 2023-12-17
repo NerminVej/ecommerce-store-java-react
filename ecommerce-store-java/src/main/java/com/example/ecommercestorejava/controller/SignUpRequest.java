4434f2f4a3b928918a26b81fd21f9c477bfeeab8
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-12-17 09:10:40 +0100

    created the signuprequest dto

---
+++ ecommerce-store-java/src/main/java/com/example/ecommercestorejava/controller/SignUpRequest.java
@@ -0,0 +1,18 @@
+package com.example.ecommercestorejava.controller;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpRequest {
+    String firstName;
+    String lastName;
+    String email;
+    String password;
+}
